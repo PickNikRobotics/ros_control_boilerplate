470c8e716b953cd9925f87a21c00977901bdee8d
Tim Übelhör <t.uebelhoer@irt.rwth-aachen.de>
2020-11-23 16:07:28 +0100

    Replaced boost with std shared_ptr

--- src/sim_hw_main.cpp
+++ src/sim_hw_main.cpp
@@ -50,7 +50,7 @@ int main(int argc, char** argv)
   spinner.start();
 
   // Create the hardware interface specific to your robot
-  boost::shared_ptr<ros_control_boilerplate::SimHWInterface> sim_hw_interface
+  std::shared_ptr<ros_control_boilerplate::SimHWInterface> sim_hw_interface
     (new ros_control_boilerplate::SimHWInterface(nh));
   sim_hw_interface->init();
 
